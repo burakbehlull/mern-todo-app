9287db95f7c58e33c1d2f037a9c576fd89a87c98
31000s <burakyabgu@gmail.com>
2023-02-28 02:19:37 +0300

    feat: Setting the Home Page

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("Im Buvak'ın sunumuyla..");
+})
+
+const port = process.env.PORT || 80;
+app.listen(port)
